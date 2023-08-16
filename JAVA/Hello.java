7c3c56cea77e823a1714681d5db461a7af3d16d3
kdhzoot <kdhzoot@gmail.com>
2023-08-16 23:02:18 +0900

    studying collections

--- JAVA/Hello.java
+++ JAVA/Hello.java
@@ -1,6 +1,36 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        HashMap map = new HashMap();
+        map.put("myId", "1234");
+        map.put("asdf", "1111");
+        map.put("fdsa", "1234");
+
+        Scanner s = new Scanner(System.in);
+
+        while(true){
+            System.out.println("id와 password를 입력해주세요");
+            System.out.print("id : ");
+            String id = s.nextLine().trim();
+
+            System.out.print("password :");
+            String password = s.nextLine().trim();
+            System.out.println();
+
+            if(!map.containsKey(id)){
+                System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요");
+                continue;
+            }else{
+                if(!map.get(id).equals(password)){
+                    System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요");
+                }else{
+                    System.out.println("로그인 되었습니다");
+                    break;
+                }
+            }
+        }
     }
 }
 
